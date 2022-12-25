229fe3e8a350f34f8055c4e5462009b04abbec19
skYth1an <Elgatomolli27072908>
2022-12-25 21:28:45 +0300

    add README.15_2.md

--- manifests/main.tf
+++ manifests/main.tf
@@ -21,6 +21,7 @@ resource "yandex_compute_instance" "default" {
   resources {
     cores  = 2
     memory = 2
+    core_fraction = 20
   }
 
   boot_disk {
@@ -37,64 +38,246 @@ resource "yandex_compute_instance" "default" {
 
 }
 
-#PUBLIC Instance
-resource "yandex_compute_instance" "public-nat" {
-  name        = "public-nat"
-  zone        = "ru-central1-a"
+
+resource "yandex_iam_service_account" "sa" {
+  name = "storageman"
+}
+
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = "b1gjmrhngcisglrqmhda"
+  role      = "admin"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
 
 
-  resources {
-    cores  = 2
-    memory = 2
+resource "yandex_storage_bucket" "bucket" {
+  bucket     = "lebedev25.12.22"
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+
+  anonymous_access_flags {
+    read = true
+    list = true
+  }
+
+}
+
+resource "yandex_storage_object" "storage" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "lebedev25.12.22"
+  key        = "1.jpg"
+  source     = "1.jpg"
+}
+
+resource "yandex_storage_object" "storage2" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "lebedev25.12.22"
+  key        = "index.html"
+  source     = "index.html"
+}
+
+
+#Группа хостов
+resource "yandex_compute_instance_group" "group1" {
+  name                = "group1"
+  folder_id = "b1gjmrhngcisglrqmhda"
+  service_account_id  = "${yandex_iam_service_account.sa.id}"
+  deletion_protection = false
+  instance_template {
+    resources {
+      memory = 2
+      cores  = 2
+      core_fraction = 20
     }
+    boot_disk {
+      initialize_params {
+        image_id = "fd827b91d99psvq5fjit"
+      }
+    }
+    network_interface {
+      subnet_ids = ["${yandex_vpc_subnet.public.id}"]
+      nat = true
+    }
+    labels = {
+      label1 = "label1-value"
+      label2 = "label2-value"
+      label3 = "label3-value"
+    }
+    metadata = {
+      user-data = "${file("data.txt")}"
+      #ssh-keys = "ubuntu:${file("/home/vagrant/.ssh/id_rsa.pub")}"
+    }
+    network_settings {
+      type = "STANDARD"
+    }
+  }
 
-  network_interface {
+  variables = {
+    test_key1 = "test_value1"
+    test_key2 = "test_value2"
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 3
+    }
+  }
+
+  allocation_policy {
+    zones = ["ru-central1-a"]
+  }
+
+  deploy_policy {
+    max_unavailable = 3
+    max_creating    = 3
+    max_expansion   = 3
+    max_deleting    = 3
+  }
+}
+
+
+resource "yandex_lb_target_group" "tgt" {
+  name      = "tgt"
+
+  target {
     subnet_id = "${yandex_vpc_subnet.public.id}"
-    nat = true
+    address   = "192.168.10.21"
   }
 
-  metadata = {
-    ssh-keys = "ubuntu:${file("/home/vagrant/.ssh/id_rsa.pub")}"
+  target {
+    subnet_id = "${yandex_vpc_subnet.public.id}"
+    address   = "192.168.10.36"
   }
 
-  boot_disk {
-    initialize_params {
-      image_id = "fd8b24tqvq7t2f8a1o1s"
+  target {
+    subnet_id = "${yandex_vpc_subnet.public.id}"
+    address   = "192.168.10.13"
+  }
+
+}
+
+resource "yandex_lb_network_load_balancer" "balancer" {
+  name = "balancer"
+
+  listener {
+    name = "my-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
     }
   }
 
+  attached_target_group {
+    target_group_id = "${yandex_lb_target_group.tgt.id}"
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
+}
+
+resource "yandex_alb_target_group" "tgt2" {
+  name      = "tgt2"
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.public.id}"
+    ip_address  = "192.168.10.21"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.public.id}"
+    ip_address   = "192.168.10.36"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.public.id}"
+    ip_address   = "192.168.10.13"
+  }
 
 }
 
 
+resource "yandex_alb_backend_group" "backendgroup" {
+  name      = "backendgroup"
 
-#PRIVATE Instance
-resource "yandex_compute_instance" "private-nat" {
-  name        = "private-nat"
-  zone        = "ru-central1-a"
-
-
-  resources {
-    cores  = 2
-    memory = 2
+  http_backend {
+    name = "test-http-backend"
+    weight = 1
+    port = 80
+    target_group_ids = ["${yandex_alb_target_group.tgt2.id}"]
+    load_balancing_config {
+      panic_threshold = 50
     }
-
-  network_interface {
-    subnet_id = "${yandex_vpc_subnet.private.id}"
-
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd8b24tqvq7t2f8a1o1s"
+    healthcheck {
+      timeout = "1s"
+      interval = "1s"
+      http_healthcheck {
+        path  = "/"
+      }
     }
+    http2 = "false"
   }
-
-  metadata = {
-    user-data = "${file("data.txt")}"
-  }
-
 }
+
+resource "yandex_alb_http_router" "tf-router" {
+  name   = "tf-router"
+}
+
+resource "yandex_alb_virtual_host" "virtual-host" {
+  name           = "virtual-host"
+  http_router_id = "${yandex_alb_http_router.tf-router.id}"
+  route {
+    name = "my-route"
+    http_route {
+      http_route_action {
+        backend_group_id = "${yandex_alb_backend_group.backendgroup.id}"
+        timeout          = "3s"
+      }
+    }
+  }
+}
+resource "yandex_alb_load_balancer" "balancerl7" {
+  name        = "balancerl7"
+  network_id  = "${yandex_vpc_network.network.id}"
+
+  allocation_policy {
+    location {
+      zone_id   = "ru-central1-a"
+      subnet_id = "${yandex_vpc_subnet.public.id}"
+    }
+  }
+
+  listener {
+    name = "listenerl7"
+    endpoint {
+      address {
+        external_ipv4_address {
+        }
+      }
+      ports = [ 9000 ]
+    }
+    http {
+      handler {
+        http_router_id = "${yandex_alb_http_router.tf-router.id}"
+      }
+    }
+  }
+}
+
+
 
 
 
@@ -125,6 +308,8 @@ resource "yandex_vpc_route_table" "route-table" {
 
   static_route {
     destination_prefix = "0.0.0.0/0"
-    next_hop_address   = "192.168.10.254"
+    next_hop_address   = "${yandex_compute_instance.default.network_interface[0].ip_address}"
   }
+
 }
+
